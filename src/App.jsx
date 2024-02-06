428885a669eb59133862dcd3c9301fb68f3f2033
Pawan <pawanb78@gmail.com>
2024-02-06 23:37:55 +0000

    final project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import RandomColor from './Components/RandomColor'
+
+function App() {
+
+
+  return (
+    <>
+    <div className="container">
+        <RandomColor/>
+    </div>
+    </>
+  )
+}
+
+export default App
